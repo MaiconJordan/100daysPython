4c74c358e682258e0c666df316408bad0e8d896d
Maicon <maiconplus@gmail.com>
2023-01-20 09:53:36 -0300

    ex005

---
+++ exercicioCursoEmVideo/ex005.py
@@ -0,0 +1,8 @@
+#programa que mostra antecessor e sucessor do numero
+n1 = int(input('Digite um numero: '))
+s = n1+1
+a = n1-1
+print('O nomero {} tem como sucessor o {} e o antecessor o {}'.format(n1, s, a))
+
+
+

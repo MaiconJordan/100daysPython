4d4cd7ad7148ffa121bff44fefa98c459c8cb0d5
Maicon <maiconplus@gmail.com>
2023-01-20 22:19:07 -0300

    day 1 final

---
+++ day1_final.py
@@ -0,0 +1,6 @@
+#programa pra gerar nome de banda
+
+nome_cidade = input('Qual o nome da cidade que você nasceu?\n')
+nome_pet = input('Qual o nome do seu primeiro pet? \n')
+
+print('O nome da sua banda vai ser {} {} '.format(nome_pet, nome_cidade))

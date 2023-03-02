39b9512ea5b561ed9331f9565de202e16f366d56
Maicon <maiconplus@gmail.com>
2023-03-02 14:51:37 -0300

    index de listas

--- day4-1.py
+++ day4-1.py
@@ -1,9 +1,9 @@
+# programa que pega um nome aleatorio da lista
 import random
 
 nomes_string = input("Digite o nome dos integrantes que vão pagar a conta: ")
 
 nomes = nomes_string.split(", ")
-
 
 n = len(nomes)
 
@@ -13,4 +13,6 @@ print(num_lista)
 
 print(nomes[num_lista])
 
+
+# mesmo programa agora com a função choice do random
 
---
+++ day4-2.py
@@ -0,0 +1,24 @@
+#Exercicio de listas
+linha1 = ["o","o","o"]
+linha2 = ["o","o","o"]
+linha3 = ["o","o","o"]
+
+quadro = [linha1 , linha2, linha3]
+print("  1     2      3")
+print(f"1{linha1}\n2{linha2}\n3{linha3}")
+
+linha_escolinha = input("Veja o quadro acima, digite qual bola você quer acertar.\nutilando primeiro a coluna e depois a linha ex: 21  (coluna 2 linha 1): ")
+
+coluna = int(linha_escolinha[0])
+linha = int(linha_escolinha[1])
+
+linha_s = quadro[linha - 1]
+linha_s[coluna - 1] = "X"
+
+
+print("  1     2      3")
+print(f"1{linha1}\n2{linha2}\n3{linha3}")
+
+
+
+

3b78daa1b4a79476d521ebbbdf2064a6ac1914bd
Maicon <maiconplus@gmail.com>
2023-03-09 23:08:27 -0300

    media e numero de itens na lista

---
+++ day5.py
@@ -0,0 +1,13 @@
+numeros = input("Digite os numeros que você quer a média: ").split()
+
+for n in range(0, len(numeros)):
+    n += 1
+soma = 0
+for i in numeros:    
+    num_int = int(i)
+    soma += num_int
+media =  soma / len(numeros)
+print(media)
+print(soma)
+print(n)
+#print(media)

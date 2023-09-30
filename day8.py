6bf894a692614f23e3f8b38ce40d85881a619940
Maicon <maiconplus@gmail.com>
2023-09-29 22:46:36 -0300

    exercicio funcao

--- day8.py
+++ day8.py
@@ -1,5 +1,17 @@
-def greet_with(one, two):
-    print(one, two)
+#ef greet_with(one, two):
+ #   print(one, two)
 
-greet_with("Gol", 3)
-greet_with(two = 3, one = "Gol")
+#greet_with("Gol", 3)
+#greet_with(two = 3, one = "Gol")
+
+
+h = int(input("Digite a altura: "))
+w = int(input("Digite a largura: "))
+redimento = 5
+
+def caluculo_area(h, w):
+    area = h * w
+    redimento_latas = area / 5        
+    print(f"Você vai precisar de {round(redimento_latas)} latas")
+
+caluculo_area(h, w)

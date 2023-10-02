7903922f1aad319e8e57b359fc2cd52ffcff8cf9
Maicon <maiconplus@gmail.com>
2023-10-02 20:52:34 -0300

    abrir arquivo

--- data_science_academy/cap05-3.py
+++ data_science_academy/cap05-3.py
@@ -7,7 +7,7 @@ for num in range(2, 31):
     
     # Variável de controle
     eh_primo = True
-    
+        
     # Loop while para verificar se o número é primo
     i = 2
     while i <= num // 2:
---
+++ data_science_academy/cap06-1.py
@@ -0,0 +1,2 @@
+arq1 = open("/home/maicon.rocha@jtech.corp/Documentos/Github/100daysPython/data_science_academy/arquivos/arquivo1.txt", "r")
+print(arq1.read())

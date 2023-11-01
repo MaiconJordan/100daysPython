5b039b382b5edd0b0fb003426700e01a556bc950
Maicon <maiconplus@gmail.com>
2023-10-31 21:38:51 -0300

    projeto uf_game

---
+++ projetos_pessoais/acerte-as-uf/acete-uf.py
@@ -0,0 +1,29 @@
+print("\nACERTE TODAS AS UF(ESTADOS) DO BRASIL\n")
+
+acertos = []
+
+ufs = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+
+uf_jorgador = []
+
+
+while True:
+
+    uf = input("\nDIGITE UMA UF: ")  
+    
+    
+    
+    if uf.upper() in ufs and uf.upper() not in uf_jorgador: 
+        uf_jorgador.append(uf.upper())
+        acertos.append(uf.upper()) 
+        print()
+        print("ACERTOS: ", " ".join(acertos))
+    else: 
+        print("UF INVALIDA OU JA DIGITADA")        
+   
+
+    
+    
+
+        
+

17f9fb0569c245dea3655f7b93f965a1a7317afd
Maicon <maiconplus@gmail.com>
2023-11-22 21:42:39 -0300

    funçao que compara n seguidores

--- Udemy/day14_project/day14.py
+++ Udemy/day14_project/day14.py
@@ -3,20 +3,33 @@ import random
 from insta_dados import instagram_accounts
 
 def adversario_um():
-    n = random.randint(1, 51)
-    adv = instagram_accounts[n]
-    return adv['nome'], adv['seguidores']
+    n = random.randint(0, 49)    
+    adv = instagram_accounts[n]    
+    return str(adv['nome']), str(adv['seguidores'])
     
 
 def adversario_dois():
-    n = random.randint(1, 51)
-    adv = instagram_accounts[n]
-    return adv['nome'], adv['seguidores']
+    n = random.randint(0, 49)    
+    adv = instagram_accounts[n]    
+    return str(adv['nome']), str(adv['seguidores'])
+
+
+def compara(insta_um, insta_dois):
+
+    if insta_um > insta_dois:
+        return insta_um
+    else:
+        return insta_dois
     
+tentativas = 3
 
+while tentativas <= 3:  
+    um = adversario_um()
+    dois = adversario_dois()
+    resultado = compara(um, dois)
 
-
-
-um = adversario_um()
-
-print(um)
+    print(' '.join(um))    
+    print(' '. join(dois))
+    input('Quem tem mais seguidor ? ')
+    print(f'Quem tem mais seguidor é: {resultado[0]} com {float(resultado[1])} seguidores\n')
+    tentativas =- 1

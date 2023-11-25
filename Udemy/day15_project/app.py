1248b762398c13fa03fad1f04dc27ee7812564c7
Maicon <maiconplus@gmail.com>
2023-11-24 23:24:12 -0300

    funcoes principais

--- Udemy/day15_project/app.py
+++ Udemy/day15_project/app.py
@@ -1,1 +1,27 @@
-print("O que você gostaria? (espresso/latte/cappuccino):")
+agua = 300
+leite = 200
+cafe = 100
+saldo = 0
+
+def verificar_recurso():
+    print(f"Água: {agua}ml\n Leite: {leite}ml\n Café: {cafe}g\n Dinheiro: ${saldo}")
+    
+
+def processar_moedas():
+    pass
+
+def processar_pagamento():
+    pass
+
+
+def fazer_cafe():
+    pass
+
+
+solicitacao = input("O que você gostaria? (espresso/latte/cappuccino):")
+
+verificar_recurso()
+
+
+
+

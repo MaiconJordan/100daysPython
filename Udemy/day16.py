d356e6a90bfbe16248fb3ee20141e262d0365790
Maicon <maiconplus@gmail.com>
2023-11-26 21:52:23 -0300

    aula POO

---
+++ Udemy/day16.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("black", "green")
+timmy.forward(100)
+
+
+my_screan = Screen()
+print(my_screan.canvheight)
+my_screan.exitonclick()

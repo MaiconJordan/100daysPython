e8d1ab6280ba3adad047fc3486ecc4b34261be20
Maicon <maiconplus@gmail.com>
2023-11-29 21:44:13 -0300

    method still_has_question

--- Udemy/day17_project/quiz_brain.py
+++ Udemy/day17_project/quiz_brain.py
@@ -3,6 +3,11 @@ class QuizBrain:
         self. question_number = 0
         self.question_list = q_list
 
+    def still_has_questions(self):
+       if self.question_number < len(self.question_list):
+          return True
+       else:
+          return False
     
     def next_question(self):        
       for i in range(len(self.question_list)):  

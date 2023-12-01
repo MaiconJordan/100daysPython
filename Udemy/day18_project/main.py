4340a4b329ac419397cd48651a7ad2e40b7c5718
Maicon <maiconplus@gmail.com>
2023-11-30 21:31:02 -0300

    turtle

---
+++ Udemy/day18_project/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.rt()
+timmy_the_turtle.forward(100)
+timmy_the_turtle.rt()
+timmy_the_turtle.bk(100)
+
+
+
+screen = Screen()
+screen.exitonclick()
